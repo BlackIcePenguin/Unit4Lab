3b1d500255070d2730fbf7430aea952c55e77e74
BlackIcePenguin <bonneyjack10@gmail.com>
2021-09-29 09:20:50 -0400

    Trees move

--- Unit4Lab.py
+++ Unit4Lab.py
@@ -31,6 +31,7 @@ pygame.display.set_caption('Pygame Lab')
 clock = pygame.time.Clock()
 day_night = 0
 am = True
+tree_shift = 400
 
 
 # Setting the sky loop color
@@ -40,11 +41,11 @@ def sky_color(time):
 # Tree, based on bottom right point on tree for x
 
 
-def tree_builder(x, y):
+def tree_builder(x, y, tree_change):
     pygame.draw.rect(screen, BROWN, [x-20, y-80, 20, 80], width=0)
     for number in range(0, 2):
-        pygame.draw.polygon(screen, DARK_GREEN, [(x-60, y-60-60*number), (x+40, y-60-60*number),
-                                                 (x-10, y-180-(60*number))], width=0)
+        pygame.draw.polygon(screen, DARK_GREEN, [(x-60, tree_shift-60-60*number), (x+40, tree_shift-60-60*number),
+                                                 (x-10, tree_shift-180-(60*number))], width=0)
 
 
 running = True
@@ -71,13 +72,18 @@ while running:
     elif day_night == 0 and am is False:
         am = True
         day_night += 1
+    # Trees moving via sky cycle
+    if am is True:
+        tree_shift += 1/36
+    elif am is False:
+        tree_shift -= 1/36
 
     # Add drawings here
     pygame.draw.rect(screen, GREEN, [0, 400, 700, 500], width=0)
     for num in range(0, 4):
         pygame.draw.ellipse(screen, WHITE, [80 + 180*num, 100 - 10*num, 120, 60], width=0)
     for val in range(0, 6):
-        tree_builder(70 + 115*val, 400)
+        tree_builder(70 + 115*val, 400, tree_shift)
 
     pygame.display.flip()
 
